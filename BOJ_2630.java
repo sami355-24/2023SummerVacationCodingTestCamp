0c0cc48280f755d78dd5d90ffae018e127263da6
namgigun <namgigun@gmail.com>
2023-07-11 16:22:22 +0900

    백준 2630 (완)

--- BOJ_2630.java
+++ BOJ_2630.java
@@ -1,92 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_2630 {
-    static int count[] = {0,0};
+    static int[][] arr;
+    static int white = 0; // 흰색 색종이 개수
+    static int blue = 0; // 파란색 색종이 개수
+
     public static void main(String[] args) throws IOException{
         int n;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine()); // 배열 정보
+        n = Integer.parseInt(br.readLine()); // 한 변의 길이
 
-        int[][] arr = new int[n + 1][n + 1];
+        arr = new int[n + 1][n + 1];
 
-        for(int i = 1; i <= n; i++) { // 입력
+        StringTokenizer st;
+
+        for(int i = 1; i <= n; i++) { // 입력값 삽입
+            st = new StringTokenizer(br.readLine(), " ");
+
             for(int j = 1; j <= n; j++) {
-                arr[i][j] = Integer.parseInt(br.readLine());
+                arr[i][j] = Integer.parseInt(st.nextToken());    
             }
         }
 
-        sol(n, n, arr);
+        sol(1, 1, n);
+
+        System.out.println(white);
+        System.out.println(blue);
     }
     
-    static void sol(int x , int y, int[][] arr) {
-        // x의 절편, y의 절편을 알 수 있음.
-        // 중간점
-        int mid_x = x / 2;
-        int mid_y = y / 2;
+    static void sol(int x , int y, int size) {
 
-        int[] chk = {0,0,0,0}; // 4개의 부분 (모두 1인 경우 1, 모두 0인 경우 0, 그게 아닌 경우 2)
-
-        int init = arr[mid_x][mid_y];
-
-        for(int i = 1; i <= mid_x; i++) {
-            for(int j = 1; j <= mid_y; j++) {
-                if(arr[i][j] != init) {
-                    chk[0] = 2;
-                }        
+        if(checkColor(x,y,size) == true) {
+            if(arr[x][y] == 0) {
+                white++;
             }
-        }
 
-        if(chk[0] != 2) {
-            count[init] += 1;
-        }
-
-        else {
-            
-        }
-
-        init = arr[mid_x][mid_y + 1];
-
-        for(int i = 1; i <= mid_x; i++) {
-            for(int j = mid_y + 1; j <= y; j++) {
-                if(arr[i][j] != init) {
-                    chk[1] = 2;
-                }        
+            else {
+                blue++;
             }
+
+            return;
         }
 
-        if(chk[1] != 2) {
-            count[init] += 1;
-        }
+        int halfSize = size / 2;
 
-        init = arr[mid_x + 1][mid_y];
+        sol(x, y, halfSize);
+        sol(x , y + halfSize, halfSize);
+        sol(x + halfSize, y, halfSize);
+        sol(x + halfSize, y + halfSize, halfSize);
+    }
 
-        for(int i = mid_x + 1; i <= x; i++) {
-            for(int j = 1; j <= mid_y; j++) {
-                if(arr[i][j] != init) {
-                    chk[2] = 2;
-                }
-            }
-        }
+    static Boolean checkColor(int x, int y, int size) { // 영역안에 다른 색깔이 있는 지 확인.
+        int init = arr[x][y]; // 초기값 설정
         
-        if(chk[2] != 2) {
-            count[init] += 1;
-        }
-
-        init = arr[mid_x + 1][mid_y + 1];
-
-        for(int i = mid_x + 1; i <= x; i++) {
-            for(int j = mid_y + 1; j <= y; j++) {
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
                 if(arr[i][j] != init) {
-                    chk[3] = 2;
+                    return false;
                 }
             }
         }
 
-        if(chk[3] != 2) {
-            count[init] += 1;
-        }
+        return true;
     }
 }
