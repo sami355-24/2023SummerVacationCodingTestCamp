d6eb8c3d3217cca90d62e1bdb58a0a9ecd36ce21
namgigun <namgigun@gmail.com>
2023-07-16 11:05:21 +0900

    백준 12919 (실패)

--- BOJ_12919.java
+++ BOJ_12919.java
@@ -13,7 +13,18 @@ public class BOJ_12919 {
         System.out.println(canChange);
     }
 
-    static int sol(String s, String T) {
+    static int sol(String S, String T) {
+        StringBuffer sb = new StringBuffer(S);
+        String reverse_S = sb.reverse().toString();
+
+        if(!(T.contains(S) || T.contains(reverse_S))) {
+            return 0;
+        }
+
+        if(T.charAt(0) == 'A' && T.contains("B")) {
+            return 0;
+        }
+
         return 1;
     }
 }
