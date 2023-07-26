38afa841f305e308635b096aae2817a2ed61b089
namgigun <namgigun@gmail.com>
2023-07-26 15:56:42 +0900

    BOJ_16139 (완)

--- BOJ_16139.java
+++ BOJ_16139.java
@@ -6,36 +6,39 @@ import java.util.StringTokenizer;
 public class BOJ_16139 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s = br.readLine();
-        int Qcount = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        int Qcount = Integer.parseInt(br.readLine());        
+        int slen = s.length();
+        int[][] sum = new int[slen][26];
 
-        char at;
-        int start;
-        int end;
+        sum[0][s.charAt(0) - 'a']++;
 
-        for(int i = 0; i < Qcount; i++) {
-            String input = br.readLine();
-            st = new StringTokenizer(input, " ");
-            at = st.nextToken().charAt(0);
-            start = Integer.parseInt(st.nextToken());
-            end = Integer.parseInt(st.nextToken());
-            String t = s.substring(start, end + 1);
-            System.out.println(sol(t,at));
+        for(int i = 1; i < slen; i++) {
+            int tmp = s.charAt(i) - 'a';
+            
+            for(int j = 0; j < 26; j++) {
+                sum[i][j] = sum[i - 1][j];
+            }
+
+            sum[i][tmp]++;
         }
-    }
 
-    static int sol(String t, char at) {
-        int count = 0;
+        StringBuilder sb = new StringBuilder(); // 이거떄매 50점...?
+        for(int i = 0; i < Qcount; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            char at = st.nextToken().charAt(0);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < t.length(); i++) {
-            if(t.charAt(i) == at) {
-                count++;
+            if(start == 0) {
+                sb.append(sum[end][at - 'a']).append('\n');
+            }
+
+            else {
+                sb.append(sum[end][at - 'a'] - sum[start - 1][at - 'a']).append('\n');
             }
         }
-
-        return count;
+        System.out.println(sb);
     }
 }
