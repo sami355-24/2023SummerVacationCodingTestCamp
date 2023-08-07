b48988e3c927c857b9aac441a39f176b356d587d
namgigun <namgigun@gmail.com>
2023-08-07 17:31:51 +0900

    백준 2302 (완)

--- 백준 (기은)/nge_2302.java
+++ 백준 (기은)/nge_2302.java
@@ -1,71 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class nge_2302 {
-    static boolean[] selected;
-    static boolean[] chs;
-    static int count = 0;
     static int n;
-    static int[] saveFix;
+    static List<Integer> list = new ArrayList<Integer>();
+    static int[] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine()); // 좌석의 개수
         int m = Integer.parseInt(br.readLine()); // 고정석의 개수
-
-        selected = new boolean[n + 1];
-        chs = new boolean[n + 1];
-        saveFix = new int[m];
-        chs[0] = true;
-
+        
         for(int i = 0; i < m; i++) {
             int fix = Integer.parseInt(br.readLine());
-            saveFix[i] = fix;
-            selected[fix] = true;
-        } // 고정석 추가
+            list.add(fix);
+        }
 
-        sol(1);
+        dp = new int[n + 1];
 
-        System.out.println(count);
+        System.out.println(sol());
     }
 
-    static void init() {
-        selected = new boolean[n + 1];
-        chs[0] = true;
+    static int sol() {
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i : saveFix) {
-            selected[i] = true;
+        for(int i = 2; i <= n; i++) {
+            if(list.contains(i) || list.contains(i - 1)) {
+                dp[i] = dp[i - 1];
+            }
+
+            else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
         }
-    }
-
-    static void sol(int loc) {
-        if(loc == n) {
-            count++;
-            return;
-        }
-
-        if(selected[loc] == true) { // 지정석
-            chs[loc] = true;
-            sol(loc + 1);
-            chs[loc] = false;
-        }
-
-        else { // 지정석이 아닌 경우
-           if(!selected[loc - 1] && !chs[loc - 1]) {
-                chs[loc - 1] = true;
-                sol(loc + 1);
-                chs[loc - 1] = false;
-           }
-
-           if(!selected[loc + 1] && !chs[loc + 1]) {
-                chs[loc + 1] = true;
-                sol(loc + 1);
-                chs[loc + 1] = false;
-           }
-
-            chs[loc] = true;
-            sol(loc + 1);
-            chs[loc] = false;
-        }
+        
+        return dp[n];
     }
 }
