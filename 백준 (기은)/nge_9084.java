d90fd1789425215552402a50f19fa91031fee72c
namgigun <namgigun@gmail.com>
2023-08-23 16:18:52 +0900

    백준 9084 (완)

--- 백준 (기은)/nge_9084.java
+++ 백준 (기은)/nge_9084.java
@@ -5,35 +5,47 @@ import java.util.StringTokenizer;
 
 public class nge_9084 {
     static int n; // 동전 가지 수
-    static int[][] coin; // 동전 종류 + 동전 개수
-    static int[] limit; // 각 동전의 한계치
-    static int count;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+    static int[] coin; // 동전 종류 + 동전 개수
+    static int m; // 동전으로 만들어야 할 금액
+    static int[] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
-        
+        StringBuilder sb = new StringBuilder();
+
         for(int i = 0; i < t; i++) {
             n = Integer.parseInt(br.readLine());
-            coin = new int[n + 1][2];
-            limit = new int[n + 1];
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(br.readLine());
+            m = Integer.parseInt(br.readLine());
 
-            for(int j = 1; j <= n; j++) {
-                int val = Integer.parseInt(st.nextToken());
-                coin[j][0] = val;
-                limit[j] = m / val;
-                count = 0;
-                sol();
+            coin = new int[n];
+            dp = new int[m + 1];
+            dp[0] = 1;
+
+            for(int j = 0; j < n; j++) {
+                coin[j] = Integer.parseInt(st.nextToken());  
             }
+
+            sol();
+
+            sb.append(dp[m] + "\n");
         }
+
+        System.out.println(sb);
     }
 
-    // 0001 0002 0003 
     static void sol() {
-        coin
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(j - coin[i] > 0) {
+                    dp[j] += dp[j - coin[i]];
+                }
+
+                else if(j - coin[i] == 0) {
+                    dp[j] += 1;
+                }
+            }
+        }
     }
 }
