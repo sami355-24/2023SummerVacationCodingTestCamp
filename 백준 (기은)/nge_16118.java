95bded600b085557f8c2089799f1a1a2ea605f46
namgigun <namgigun@gmail.com>
2023-08-30 01:55:50 +0900

    백준 16118 (실패)

--- 백준 (기은)/nge_16118.java
+++ 백준 (기은)/nge_16118.java
@@ -8,16 +8,33 @@ import java.util.StringTokenizer;
 
 class Node implements Comparable<Node>{
     int index;
-    int cost;
+    double cost;
 
-    public Node(int index, int cost) {
+    public Node(int index, double cost) {
         this.index = index;
         this.cost = cost;
     }
 
     @Override
     public int compareTo(Node o) {
-        return Integer.compare(this.cost, o.cost);
+        return Double.compare(this.cost, o.cost);
+    }
+}
+
+class NodeWolf implements Comparable<NodeWolf> {
+    int index;
+    double cost;
+    Boolean isRest;
+
+    public NodeWolf(int index, double cost, boolean isRest) {
+        this.index = index;
+        this.cost = cost;
+        this.isRest = isRest;
+    }
+
+    @Override
+    public int compareTo(NodeWolf o) {
+        return Double.compare(this.cost, o.cost);
     }
 }
 
@@ -27,8 +44,8 @@ public class nge_16118 {
 
     static int count = 0;
     static ArrayList<Node>[] graph; // 그래프
-    static int distFox[];
-    static int distWolf[];
+    static double distFox[];
+    static double distWolf[];
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,17 +63,24 @@ public class nge_16118 {
             int cost = Integer.parseInt(st.nextToken());
 
             graph[v].add(new Node(w, cost));
+            // graph[w].add(new Node(v, cost));
         }
 
         sol();
+
+        for(int i = 1; i <= stumpCnt; i++) {
+            if(distFox[i] < distWolf[i]) {
+                System.out.println(distWolf[i]);
+            }
+        }
 
         System.out.println(count);
     }
 
     static void sol() {
         boolean[] chk = new boolean[stumpCnt + 1];
-        distFox = new int[stumpCnt + 1];
-        int INF = Integer.MAX_VALUE;
+        distFox = new double[stumpCnt + 1];
+        double INF = Double.MAX_VALUE;
 
         Arrays.fill(distFox, INF);
         distFox[1] = 0;
@@ -73,7 +97,41 @@ public class nge_16118 {
             for(Node next : graph[nowVertex]) {
                 if(distFox[next.index] > distFox[nowVertex] + next.cost) { // 여우
                     distFox[next.index] = distFox[nowVertex] + next.cost;
-                    pq.offer(new Node(next.cost, distFox[next.index]));
+                    pq.offer(new Node(next.index, distFox[next.index]));
+                }
+            }
+        }
+
+        PriorityQueue<NodeWolf> pqW = new PriorityQueue<>();
+        pqW.offer(new NodeWolf(1,0, false));
+
+        chk = new boolean[stumpCnt + 1];
+        distWolf = new double[stumpCnt + 1];
+        Arrays.fill(distWolf, INF);
+        distWolf[1] = 0;
+        boolean isRest;
+
+        while(!pqW.isEmpty()) {
+            NodeWolf now = pqW.poll();
+            int nowVertex = now.index;
+            isRest = now.isRest;
+
+            if(chk[nowVertex]) continue;
+            chk[nowVertex] = true;
+
+            for(Node next : graph[nowVertex]) {
+                if(isRest == false) {
+                    if(distWolf[next.index] > distWolf[nowVertex] + next.cost / 2.0) { // 여우
+                        distWolf[next.index] = distWolf[nowVertex] + next.cost / 2.0;
+                        pqW.offer(new NodeWolf(next.index, distWolf[next.index], true));
+                    }
+                }
+
+                else {
+                     if(distWolf[next.index] > distWolf[nowVertex] + next.cost * 2.0) { // 여우
+                        distWolf[next.index] = distWolf[nowVertex] + next.cost * 2.0;
+                        pqW.offer(new NodeWolf(next.index, distWolf[next.index], false));
+                    }
                 }
             }
         }
