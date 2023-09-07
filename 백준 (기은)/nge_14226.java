a0c1a7d5e3a84e7f16c46caf1757e795fb287bf9
namgigun <namgigun@gmail.com>
2023-09-07 16:40:44 +0900

    백준 14226 (완)
    블로그 참고 (BFS를 이용한 다이나믹 프로그램으로 푸는 문제)
    참조 : https://moonsbeen.tistory.com/236

--- 백준 (기은)/nge_14226.java
+++ 백준 (기은)/nge_14226.java
@@ -1,39 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class nge_14226 {
+    static class Node {
+    int curClip;
+    int curScreen;
+    int curTime;
+
+    public Node(int curClip, int curScreen, int curTime) {
+            this.curClip = curClip;
+            this.curScreen = curScreen;
+            this.curTime = curTime;
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int s = Integer.parseInt(br.readLine()); // 만들고 싶은 이모티콘 갯수
-        System.out.println(sol(s));
+        sol(s);
     }
 
-    static int sol(int s) {
-        int ans = 0;
-        int[] dp = new int[1001];
-        dp[1] = 0;
+    static void sol(int s) {
+        boolean[][] visited = new boolean[1001][1001];
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0,1,0));
+        visited[0][1] = true;
 
-        for(int i = 2; i <= 1000; i++) {
-            dp[i] = i;
-        }
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
 
-        for(int i = 4; i <= 1000; i++) {
-            if(i % 2 == 0) {
-                if(dp[i] > dp[i / 2] + 2) {
-                    dp[i] = dp[i / 2] + 2;
-                }
+            if(cur.curScreen == s) {
+                System.out.println(cur.curTime);
+                break;
+            }
+
+            // 1. 클립보드 저장
+            q.offer(new Node(cur.curScreen, cur.curScreen, cur.curTime + 1));
+
+            // 2. 붙여넣기
+            if(cur.curClip > 0 && cur.curClip + cur.curScreen <= s && !visited[cur.curClip][cur.curScreen + cur.curClip]) {
+                int total = cur.curClip + cur.curScreen;
+                q.offer(new Node(cur.curClip, total, cur.curTime + 1));
+                visited[cur.curClip][total] = true;
+            }
+
+            // 3. 하나 삭제
+            if(cur.curScreen > 0 && !visited[cur.curClip][cur.curScreen - 1]) {
+                q.offer(new Node(cur.curClip, cur.curScreen - 1, cur.curTime + 1));
+                visited[cur.curClip][cur.curScreen - 1] = true;
             }
         }
-
-        for(int i = 4; i < 1000; i++) {
-            if(dp[i] > dp[i + 1] + 1) {
-                dp[i] = dp[i + 1] + 1;
-            }
-        }
-
-        ans = dp[s];
-        
-        return ans;
     }
 }
