03d614f1862fa78907d176f54b71e02b03c5deaf
namgigun <namgigun@gmail.com>
2023-10-09 15:23:30 +0900

    백준 22942(완)
    1. 블로그 참고 (스택을 이용해서 푸는 문제)

--- 백준 (기은)/2023 Fall/nge_22942.java
+++ 백준 (기은)/2023 Fall/nge_22942.java
@@ -1,72 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class nge_22942 {
-    static List<int[]> list = new ArrayList<>(); // 원의 정보를 담은 리스트 (0 : 원의 중심 1 : 반지름)
-    static int[] max = {0,0};
+    static class Circle implements Comparable<Circle> {
+        int idx; // 원의 번호
+        int dot; // 반지름
+
+        public Circle(int idx, int dot) {
+            this.idx = idx;
+            this.dot = dot;
+        }
+        
+        @Override
+        public int compareTo(Circle o) {
+            return this.dot - o.dot;
+        }
+    }
+    
+     // 우선순위 큐 삽입 
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 원의 개수
+        PriorityQueue<Circle> pq = new PriorityQueue<>();
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++) { // 입력
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] input = new int[2];
-            input[0] = Integer.parseInt(st.nextToken());
-            input[1] = Integer.parseInt(st.nextToken());
-            
-            if(max[1] < input[1]) { // 가장 큰 원 구하기
-                max = input;
-            }
+            int dot = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
 
-            list.add(input);
+            pq.add(new Circle(i, dot - r));
+            pq.add(new Circle(i, dot + r));
         }
 
-        sol(); // 솔루션
+        sol(pq); // 솔루션
     }
 
-    static void sol() {
-        boolean chk = false; // 동접원인지 체크
-        boolean ans = true; // 출력 값
+    static void sol(PriorityQueue<Circle> pq) {
+        Stack<Circle> s = new Stack<>();
+        boolean ans = false;
 
-        // 풀이
-        for(int[] info : list) {
-            int d = Math.abs(max[0] - info[0]); // d 구하기
-            int sum = max[1] + info[1]; // rA + rB
-            int diff = Math.abs(max[1] - info[1]); // rA - rB 의 절대값
+        while(!pq.isEmpty()) {
+            Circle cur = pq.poll();
 
-            if(d == 0) {
-                if(chk == false) {
-                    chk = true;
-                    continue;
-                }
-
-                else {
-                    ans = false;
-                    break;
-                }
+            if(s.empty()) {
+                s.push(cur);
             }
 
-            else if(sum == d) {
-                ans = false;
-                break;
-            }
-
-            else if(diff == d) {
-                ans = false;
-                break;
-            }
-
-            else if(d > diff && d < sum) {
-                ans = false;
-                break;
+            else {
+                if(s.peek().idx == cur.idx) s.pop();
+                else s.push(cur);
             }
         }
 
-        System.out.println(ans ? "YES" : "NO");
+        if(s.isEmpty()) {
+            ans = true;
+        }
+        
+        System.out.print(ans ? "YES" : "NO");
     }
 }
