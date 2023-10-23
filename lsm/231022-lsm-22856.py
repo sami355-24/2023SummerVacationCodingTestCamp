c6ff41c591860aeca71288c27bc708cdad1a1976
sami355-24 <ddung355@gmail.com>
2023-10-23 14:26:31 +0900

    22856.py 복습

--- lsm/231022-lsm-22856.py
+++ lsm/231022-lsm-22856.py
@@ -1,43 +1,41 @@
 import sys
 sys.setrecursionlimit(10**9)
-input = lambda: sys.stdin.readline().rstrip()
+input = lambda : sys.stdin.readline().rstrip()
+
+L, R = 0, 1
+N = int(input())
+Tree = dict()
 
 def dfs1(now):
-    global count
+    global cnt1
     visit[now] = True
 
-    if not visit[graph[now][0]] and graph[now][0] != -1:
-        dfs1(graph[now][0])
-        count += 1
+    if Tree[now][L] != -1 and visit[Tree[now][L]] == False:
+        cnt1+=1
+        dfs1(Tree[now][L])
 
-    if not visit[graph[now][1]] and graph[now][1] != -1:
-        dfs1(graph[now][1])
-        count += 1
+    if Tree[now][R] != -1 and visit[Tree[now][R]] == False:
+        cnt1+=1
+        dfs1(Tree[now][R])
 
 def dfs2(now):
-    global count2
+    global cnt2
     visit[now] = True
 
-    if not visit[graph[now][1]] and graph[now][1] != -1:
-        dfs2(graph[now][1])
-        count2 += 1
-
-
-
-N = int(input().rstrip())
-graph = dict()
+    if Tree[now][R] != -1 and visit[Tree[now][R]] == False:
+        cnt2+=1
+        dfs2(Tree[now][R])
 
 for _ in range(N):
-    a,b,c = map(int, input().split())
-    graph[a]=[b,c]
+    a, b, c = map(int, input().split())
+    Tree[a] = [b,c]
 
 visit = [False] * (N+1)
-count = 0
+cnt1 = 0
 dfs1(1)
 
 visit = [False] * (N+1)
-count2 = 0
+cnt2 = 0
 dfs2(1)
 
-# print(count, count2)
-print(2*count - count2)
+print(2*cnt1 - cnt2)
