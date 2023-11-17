be1018ef10d95c64d8e5ee7daa68114117ebe7f9
namgigun <namgigun@gmail.com>
2023-11-17 15:37:55 +0900

    백준 11725 (완)
    1. 블로그 참고 (DFS로 푸는 문제 였음!!)

--- 백준 (기은)/2023 Fall/nge_11725.java
+++ 백준 (기은)/2023 Fall/nge_11725.java
@@ -1,41 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
+
 
 public class nge_11725 {
-    static TreeMap<Integer, Integer> map = new TreeMap<>();
+    static int[] parent;
+    static boolean[] isVisit;
+    static List<Integer> list[];
+    
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException{
-        // 트리 문제
         int nCount = Integer.parseInt(br.readLine()); // 노드의 개수
-        sol(nCount);
-    }
+        parent = new int[nCount + 1];
+        isVisit = new boolean[nCount + 1];
+        list = new ArrayList[nCount + 1];
 
-    static void sol(int nCount) throws IOException {
-        map.put(1, 1);
+        for(int i = 0; i <= nCount; i++) {
+            list[i] = new ArrayList<>();
+        }
 
         for(int i = 0; i < nCount - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
 
-            search(v1, v2);
+            list[v1].add(v2);
+            list[v2].add(v1);
         }
 
-        for(int i = 2; i <= nCount; i++) {
-            System.out.println(map.get(i));
+        sol(1);
+
+        for(int i = 2; i <= nCount; i++) { // 정답 출력
+            System.out.println(parent[i]);
         }
     }
 
-    static void search(int v1, int v2) {
-        if(map.getOrDefault(v1, null) != null) {
-            map.put(v2, v1);
-        }
+    static void sol(int idx) {
+        isVisit[idx] = true;
 
-        else {
-            map.put(v1, v2);
+        for(int e : list[idx]) {
+            if(!isVisit[e]) {
+                parent[e] = idx;
+                sol(e);
+            }
         }
     }
 }
