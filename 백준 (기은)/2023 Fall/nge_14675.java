8640649d3638124f0fbb1a9f6e9359be9ad6cb47
namgigun <namgigun@gmail.com>
2023-11-28 15:13:46 +0900

    백준 14675 (완)
    1. 트리의 성질을 이해했다면 풀 수 있는 문제였다
    (블로그 주소 : https://moonsbeen.tistory.com/271)

--- 백준 (기은)/2023 Fall/nge_14675.java
+++ 백준 (기은)/2023 Fall/nge_14675.java
@@ -1,26 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class nge_14675 {
-    static boolean graph[][]; // 그래프
+    static List<Integer> list[];
     static int N; // 정점 개수
-    static boolean[] visited;
+    
     static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine()); // 정점개수
-        graph = new boolean[N + 1][N + 1];
+        list = new ArrayList[N + 1];
 
-        for(int i = 1; i <= N - 1; i++) { // 입력
+        for(int i = 0; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < N - 1; i++) { // 입력
             StringTokenizer st = new StringTokenizer(br.readLine());
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
 
-            graph[v1][v2] = true;
-            graph[v2][v1] = true;
+            list[v1].add(v2);
+            list[v2].add(v1);
         }
 
         int q = Integer.parseInt(br.readLine());
@@ -37,71 +43,18 @@ public class nge_14675 {
     }
 
     static void sol(int t, int k) {
-        boolean tmp[][] = graph;
-        int ev; // 제외할 정점
-
-        if(t == 1) { // 정점 제거
-            for(int i = 1; i <= N; i++) {
-                tmp[k][i] = false;
-                tmp[i][k] = false;
-            }
-            ev = k;
+        if(t == 2) {
+            sb.append("yes").append("\n");
         }
 
-        else { // 간선 제거
-            int v1 = k;
-            int v2 = k + 1;
-
-            tmp[v1][v2] = false;
-            tmp[v2][v1] = false;
-
-            ev = 0;
-        }
-
-        // 트리체크
-        for(int i = 1; i <= N; i++) {
-            if(i == ev) { // 없어진 정점이라면 건너뛰기
-                continue;
-            }
-
-            visited = new boolean[N + 1];
-            DFS(tmp,i);
-            
-            boolean chk = isTree(ev);
-    
-            if(chk) {
-                sb.append("no").append("\n");
-                return;
+        else {
+            if(list[k].size() >= 2) {
+                sb.append("yes").append("\n");
             }
 
             else {
-                continue;
+                sb.append("no").append("\n");
             }
         }
-
-        sb.append("yes").append("\n");
-    }
-
-    static void DFS(boolean[][] tmp, int v) {
-        visited[v] = true;
-
-        for(int i = 1; i <= N; i++) {
-            if(!visited[i] && tmp[v][i] == true) {
-                DFS(tmp, i);
-            }
-        }
-    }
-
-    static boolean isTree(int ev) {
-        for(int i = 1; i <= N; i++) {
-            if(i == ev) {
-                continue;
-            }
-
-            if(visited[i] == false) {
-                return false;
-            }
-        }
-        return true;
     }
 }
