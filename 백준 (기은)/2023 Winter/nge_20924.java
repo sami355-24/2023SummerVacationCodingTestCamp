be802b9bdde4ec03b4925f4ecffff8f623ac78dc
namgigun <namgigun@gmail.com>
2023-12-06 16:11:08 +0900

    백준 20924 (완)

--- 백준 (기은)/2023 Winter/nge_20924.java
+++ 백준 (기은)/2023 Winter/nge_20924.java
@@ -1,24 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class nge_20924 {
     static int N; // 노드개수
-    static int[][] graph; // 트리정보
+    static ArrayList<Node>[] nodeList; // 트리정보
     static int postLength = 0; // 기둥의 길이
     static int max_Branch_Length = 0; // 최대 가지의 길이 
     static boolean[] visited;
-    static int[] branchCnt;
     static int gigaIdx;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         int root = Integer.parseInt(st.nextToken()); // 루트노드의 번호
-        graph = new int[N + 1][N + 1];
-        branchCnt = new int[N + 1];
+
+        nodeList = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) {
+            nodeList[i] = new ArrayList<>();
+        }
 
         for(int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -26,17 +30,14 @@ public class nge_20924 {
             int v2 = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
 
-            graph[v1][v2] = e;
-            graph[v2][v1] = e;
-
-            branchCnt[v1]++;
-            branchCnt[v2]++;
+            nodeList[v1].add(new Node(v2, e));
+            nodeList[v2].add(new Node(v1, e));
         }
 
         visited = new boolean[N + 1];
         gigaIdx = N; // 마지막 노드로 초기화
 
-        if(branchCnt[root] > 2) {
+        if(nodeList[root].size() > 1) {
             gigaIdx = root;
         }
 
@@ -50,34 +51,42 @@ public class nge_20924 {
     }
 
     static void findPost(int root) {
-       if(branchCnt[root] > 3) {
+       visited[root] = true; 
+       if(nodeList[root].size() > 2) {
             gigaIdx = root;
             return;
        }
 
-        visited[root] = true;
-
-        for(int i = 1; i <= N; i++) {
-            if(!visited[i] && graph[root][i] != 0) {
-                postLength += graph[root][i];
-                findPost(i);
+        for(Node n : nodeList[root]) {
+            if(!visited[n.num]) {
+                postLength += n.cost;
+                findPost(n.num);
             }
         }
     }
 
     static void findMax(int n, int length) {
-        if(branchCnt[n] <= 2) {
-            if(length > max_Branch_Length) {
-                max_Branch_Length = length;
-            }
+        visited[n] = true;
 
+        if(nodeList[n].size() < 2) {
+            max_Branch_Length = Math.max(max_Branch_Length, length);
             return;
         }
 
-        for(int i = gigaIdx + 1; i <= N; i++) {
-            if(!visited[i] && graph[n][i] != 0) {
-                findMax(i, length + graph[n][i]);
+        for(Node i : nodeList[n]) {
+            if(!visited[i.num]) {
+                findMax(i.num, length + i.cost);
             }
+        }
+    }
+
+    static class Node {
+        int num;
+        int cost;
+
+        public Node(int num, int cost) {
+            this.num = num;
+            this.cost = cost;
         }
     }
 }
