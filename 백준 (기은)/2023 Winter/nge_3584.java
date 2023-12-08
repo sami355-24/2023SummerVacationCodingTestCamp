82f9e6a303df206948bce41e55a3c2fcd0bdc969
namgigun <namgigun@gmail.com>
2023-12-08 22:17:30 +0900

    백준 3584 (완)
    1. 블로그 참고
    (https://dhbang.tistory.com/34)

--- 백준 (기은)/2023 Winter/nge_3584.java
+++ 백준 (기은)/2023 Winter/nge_3584.java
@@ -1,44 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class nge_3584 {
     static boolean[] visited;
-    static List<Integer> list[];
     static int N; // 노드 개수
     static StringBuilder sb = new StringBuilder();
-    static HashMap<Integer, Integer> map = new HashMap<>();
+    static int[] parents;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < T; i++) {
             N = Integer.parseInt(br.readLine());
-            list = new ArrayList[N + 1];
+            parents = new int[N + 1];
             visited = new boolean[N + 1];
-
-            for(int j = 1; j <= N; j++) {
-                list[j] = new ArrayList<>();
-            }
 
             for(int j = 0; j < N - 1; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int v1 = Integer.parseInt(st.nextToken());
                 int v2 = Integer.parseInt(st.nextToken());
 
-                list[v1].add(v2);
-                list[v2].add(v1);
+                parents[v2] = v1;
             }
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             int target_v1 = Integer.parseInt(st.nextToken());
             int target_v2 = Integer.parseInt(st.nextToken());
 
-            
+            sol(target_v1, target_v2);
+        }
+
+        System.out.print(sb);
+    }
+
+    static void sol(int t1, int t2) {
+        while(t1 > 0) {
+            visited[t1] = true;
+            t1 = parents[t1];
+        }
+
+        while(t2 > 0) {
+            if(visited[t2]) {
+                sb.append(t2).append("\n");
+                break;
+            }
+
+            t2 = parents[t2];
         }
     }
 }
