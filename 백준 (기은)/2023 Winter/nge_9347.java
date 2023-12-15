3838bd48ce0b0436e2732d7f2ec4938bd7f06ea5
namgigun <namgigun@gmail.com>
2023-12-15 14:24:39 +0900

    백준 9347 (완)
    1. 블로그 참고 (트리를 이용하는 것이 아닌 부모노드를 따라가면서 푸는 문제)
    주소 : https://loosie.tistory.com/608

--- 백준 (기은)/2023 Winter/nge_9347.java
+++ 백준 (기은)/2023 Winter/nge_9347.java
@@ -1,78 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class nge_9347 {
-    static ArrayList<Node>[] list;
-    static HashMap<Integer, Integer> map;
     static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] parent;
+    static int n;
+    static int k;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr;
 
         while(true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken()); // 노드 수
-            int k = Integer.parseInt(st.nextToken()); // 사촌의 수를 구해야 하는 노드의 번호
+            n = Integer.parseInt(st.nextToken()); // 노드 수
+            k = Integer.parseInt(st.nextToken()); // 사촌의 수를 구해야 하는 노드의 번호
 
-            if(n == 0) {
+            if(n == 0) { // 종료
                 break;
             }
 
             arr = new int[n + 1];
-            int idx = 1;
-            list = new ArrayList[n + 1];
+            parent = new int[n + 1];
 
-            for(int i = 1; i <= n; i++) {
-                list[i] = new ArrayList<>();
-            }
-
+            arr[0] = -1;
+            parent[0] = - 1;
+            int target = 0;
+            int idx = -1;
+            
             st = new StringTokenizer(br.readLine());
             
-            while(st.hasMoreTokens()) {
-                arr[idx++] = Integer.parseInt(st.nextToken());    
-            }
+            // 값 입력
+            for(int i = 1; i <= n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
 
-            // 초기 루트 위치와 연속된 정수 위치 
-            int root = 1;
-            int t = 2;
-            int depth = 1;
-
-            // 트리 생성
-            while(t <= idx) {
-                map.put(depth, arr[t]);
-
-                if(arr[t] == arr[t + 1]) {
-                    t++;
-                }
+                if(arr[i] == k) target = i;
                 
-                else {
-                    depth++;
-                }
+                if(arr[i] != arr[i - 1] + 1) idx++;
+
+                parent[i] = idx;
             }
 
-            for(int i = 1; i <= depth; i++) {
-                map.get
+            sol(target);
+        }
+
+        System.out.print(sb);
+    }
+
+    static void sol(int target) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(parent[i] != parent[target] && parent[parent[i]] == parent[parent[target]]) {
+                count++;
             }
-
-            sol(arr[root]);
         }
-    }
-
-    static void sol(int root) {
-        // BFS를 통해 구하고자
-    }
-
-    static class Node {
-        int idx;
-        int depth;
-
-        public Node(int idx, int depth) {
-            this.idx = idx;
-            this.depth = depth;
-        }
+        
+        sb.append(count).append("\n");
     }
 }
